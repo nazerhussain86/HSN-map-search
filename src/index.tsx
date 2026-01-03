3cbfd84fc0e6271878c5a243911277c3a6fffdfa
bibin shijoe <bibinshijoe1@gmail.com>
2026-01-03 16:13:19 +0530

    Revert "bfix"
    This reverts commit aeade4461bd3a83eb278116e00c37af0c83a56d5.

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ErrorBoundary } from "./ErrorBoundary";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-     <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 );
