aeade4461bd3a83eb278116e00c37af0c83a56d5
bibin shijoe <bibinshijoe1@gmail.com>
2026-01-03 16:02:19 +0530

    bfix

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { ErrorBoundary } from "./ErrorBoundary";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+     <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
